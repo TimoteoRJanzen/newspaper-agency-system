8fd388b537a791f1b7d585b38873233599e075e4
TimoteoRJanzen <timoteojanzen123@gmail.com>
2025-12-23 10:46:39 -0300

    feat: add Topic and Newspaper models

--- newspaper/admin.py
+++ newspaper/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
-from newspaper.models import Redactor
+from newspaper.models import (
+    Redactor,
+    Topic,
+    Newspaper,
+)
 
 admin.site.register(Redactor)
+admin.site.register(Topic)
+admin.site.register(Newspaper)
--- newspaper/models.py
+++ newspaper/models.py
@@ -1,9 +1,42 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from newspaper_agency import settings
+
 
 class Redactor(AbstractUser):
     years_of_experience = models.IntegerField()
 
     def __str__(self):
         return self.username
+
+
+class Topic(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
+class Newspaper(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    published_date = models.DateField()
+    topic = models.ForeignKey(
+        Topic,
+        on_delete=models.CASCADE,
+        related_name="newspapers"
+    )
+    publishers = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        related_name="newspapers"
+    )
+
+    class Meta:
+        ordering = ["-published_date"]
+
+    def __str__(self):
+        return self.title
