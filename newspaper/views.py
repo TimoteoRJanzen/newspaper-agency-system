26008f2cc5e2bd2ebf800a50ffc6c26ab7139cf3
TimoteoRJanzen <timoteojanzen123@gmail.com>
2025-12-23 11:38:21 -0300

    feat: add home page with basic project statistics

--- newspaper/views.py
+++ newspaper/views.py
@@ -1,3 +1,20 @@
+from django.contrib.auth import get_user_model
 from django.shortcuts import render
 
-# Create your views here.
+from newspaper.models import Newspaper, Topic, Redactor
+
+
+def index(request):
+    """Home page view for the Newspaper Agency."""
+
+    num_newspapers = Newspaper.objects.count()
+    num_topics = Topic.objects.count()
+    num_redactors = get_user_model().objects.count()
+
+    context = {
+        "num_newspapers": num_newspapers,
+        "num_topics": num_topics,
+        "num_redactors": num_redactors,
+    }
+
+    return render(request, "newspaper/index.html", context=context)
