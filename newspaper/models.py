2deb8e2658576aa38c92b5e40ed49bfb05393bcb
TimoteoRJanzen <timoteojanzen123@gmail.com>
2026-01-06 10:48:42 -0300

    refactor: prepare settings and database configuration for deploy

--- newspaper/models.py
+++ newspaper/models.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.urls import reverse
 
-from newspaper_agency import settings
+from django.conf import settings
 
 
 class Redactor(AbstractUser):
--- newspaper_agency/settings/base.py
+++ newspaper_agency/settings/base.py
@@ -13,7 +13,7 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -22,16 +22,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY","django-insecure-e)((-&wz3^dau&w^+9hsp*-a1f!%-68w9245f_-3re*)r_nof1")
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
-
-ALLOWED_HOSTS = [
-    "127.0.0.1"
-]
-
-INTERNAL_IPS = [
-    "127.0.0.1",
-]
 
 # Application definition
 
@@ -75,18 +65,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'newspaper_agency.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/6.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
